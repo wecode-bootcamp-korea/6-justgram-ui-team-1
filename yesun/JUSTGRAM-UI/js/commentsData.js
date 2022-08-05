5e9cdbd43dcd7252d7deef6096da0f0d73209934
YesunPark <lynn08082@gmail.com>
2022-08-05 17:30:14 +0900

    Feat: 미션5 mock data fetch 후 댓글출력 기능 완성

---
+++ yesun/JUSTGRAM-UI/js/commentsData.js
@@ -0,0 +1,17 @@
+const comment = [
+  {
+    userName: "wecode_gram",
+    content: "오늘도 좋은 하루 보내세요~!",
+    isLiked: true,
+  },
+  {
+    userName: "coding_lover",
+    content: "코딩은 안해..?",
+    isLiked: false,
+  },
+  {
+    userName: "coding_lover",
+    content: "ㅋㅋㅋㅋㅋ잘 쉬다와~",
+    isLiked: true,
+  },
+];
