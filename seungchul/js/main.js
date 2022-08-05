74b8691267db2706cb54e94d337732f8e1ad1e88
choiseungchul <tmdcjf152@naver.com>
2022-08-05 21:40:10 +0900

    add : 미션5 완성

--- seungchul/js/main.js
+++ seungchul/js/main.js
@@ -1,6 +1,10 @@
 const inputComments = document.querySelectorAll('.comment_input')
 const submitBtn = document.querySelectorAll('.posting_box')
 const commentList = document.querySelectorAll('.new_comment_set')
+const commentSet = document.getElementsByClassName('comment_set')
+
+const likeButton = document.querySelectorAll('.comments_like_btn')
+
 
 // 댓글BOX 제작 콜백함수
 const commentSeting = (button, index) => {
@@ -28,13 +32,10 @@ const commentSeting = (button, index) => {
     inputComments[index].value = "";
 }
 
-
 // 버튼 클릭했을때 댓글 등록
 submitBtn.forEach((button, index) => {
     button.addEventListener("click", () => {
         commentSeting(button, index)
-
-
     })
 });
 
@@ -46,3 +47,40 @@ inputComments.forEach((button, index) => {
         }
     })
 });
+
+
+// JSON 데이터 가져오기
+fetch('../data/feeds.json', {
+})
+    .then(res => res.json())
+    .then(data => {
+        let commentArray = data.comments;
+        commentArray.forEach((comment) => {
+            for (let i = 0; i < commentArray.length; i++) {
+                const divElement = document.createElement('div');
+                const userId = document.createElement('a')
+                const commentsText = document.createElement('span')
+                const likesBtn = document.createElement('button')
+
+                divElement.classList.add('comments_set');
+                userId.classList.add('user_id');
+                commentsText.classList.add('comments_contents');
+                likesBtn.classList.add('comments_like_btn');
+
+                userId.innerText = comment.id;
+                commentsText.innerText = comment.content;
+                likesBtn.innerHTML = `<i class="fa-regular fa-heart"></i>`
+
+                divElement.append(userId, commentsText)
+
+                commentSet[i].append(divElement, likesBtn);
+            }
+
+        }
+        )
+
+    })
+
+
+
+// 좋아요 버튼 클릭시 이벤트
