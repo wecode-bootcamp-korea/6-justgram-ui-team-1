0a197065ab3d065d2322320a1b87edead38a9afc
YesunPark <lynn08082@gmail.com>
2022-08-05 21:27:34 +0900

    Refactor: 미션5 수정

--- yesun/JUSTGRAM-UI/js/main.js
+++ yesun/JUSTGRAM-UI/js/main.js
@@ -56,6 +56,58 @@ Array.from(commentWriteButton).forEach((value, index) => {
   });
 });
 
+//내가짠 json댓글로직
+// let comment_list;
+
+// fetch("./data/commentsData.json", { method: "GET" })
+//   .then((res) => res.json())
+//   .then((data) => {
+//     comment_list = data;
+
+//     comment_list.forEach((comment, index) => {
+//       const liElement = document.createElement("li");
+//       const userName = document.createElement("div");
+//       const userComment = document.createElement("div");
+
+//       const comments = document.getElementsByClassName("commentList")[index];
+
+//       userName.innerText = comment.userName;
+//       userName.className = "feedTextId";
+//       userComment.innerText = comment.content;
+//       userComment.className = "feedTextContent";
+//       liElement.className = "comment";
+
+//       liElement.append(userName, userComment);
+//       comments.append(liElement);
+//     });
+//   });
+
+//하은멘토님 로직
+//const commentList = document.getElementsByClassName("commentList");
+
+fetch("./data/commentsData.json")
+  .then((res) => res.json())
+  .then((data) => {
+    let commentArray = data.comments;
+
+    commentArray.forEach((comment) => {
+      for (let i = 0; i < commentList.length; i++) {
+        let liElement = document.createElement("li");
+        let userName = document.createElement("div");
+        let userComment = document.createElement("div");
+
+        userName.innerText = comment.userName;
+        userName.className = "feedTextId";
+        userComment.innerText = comment.content;
+        userComment.className = "feedTextContent";
+        liElement.className = "comment";
+        liElement.append(userName, userComment);
+
+        commentList[i].append(liElement);
+      }
+    });
+  });
+
 //게시버튼 누르면 인풋에 입력된 값대로 댓글달기
 Array.from(commentWriteInput).forEach((value, index) => {
   commentWriteButton[index].addEventListener("click", () => {
