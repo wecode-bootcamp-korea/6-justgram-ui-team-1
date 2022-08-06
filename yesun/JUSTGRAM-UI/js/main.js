f8bd93dc0f404a5866e0ec9b28942b95f9a676a1
YesunPark <lynn08082@gmail.com>
2022-08-06 23:50:10 +0900

    Feat: 미션6 ID,PW 유효성검사 후 로그인 완성

--- yesun/JUSTGRAM-UI/js/main.js
+++ yesun/JUSTGRAM-UI/js/main.js
@@ -2,8 +2,30 @@ const commentWriteInput = document.getElementsByClassName("comment-input");
 const commentWriteButton = document.getElementsByTagName("button");
 const commentList = document.getElementsByClassName("commentList");
 
+//댓글입력 함수
+function writeComment(index) {
+  const li = document.createElement("li");
+  const userName = document.createElement("div");
+  const userComment = document.createElement("div");
+
+  userComment.innerText = commentWriteInput[index].value;
+  userName.innerText = "kim_code";
+
+  li.classList.add("comment");
+  userName.classList.add("feedTextId");
+  userComment.classList.add("feedTextContent");
+
+  li.append(userName, userComment);
+  commentList[index].append(li);
+
+  commentWriteInput[index].value = "";
+
+  commentWriteButton[index].classList.remove("comment-btn");
+  commentWriteButton[index].disabled = true;
+}
+
 //인풋에 값들어가면 게시버튼 활성화
-Array.from(commentWriteButton).forEach((value, index) => {
+Array.from(commentWriteInput).forEach((value, index) => {
   commentWriteInput[index].addEventListener("keyup", (e) => {
     const gesi = commentWriteInput[index].value;
 
@@ -18,73 +40,19 @@ Array.from(commentWriteButton).forEach((value, index) => {
 Array.from(commentWriteInput).forEach((value, index) => {
   commentWriteInput[index].addEventListener("keyup", (e) => {
     if (e.keyCode === 13 && commentWriteInput[index].value) {
-      const li = document.createElement("li");
-      const commentUserId = document.createElement("div");
-      const div = document.createElement("div");
-
-      li.classList.add("comment");
-      commentUserId.innerText = "kim_code";
-      commentUserId.classList.add("feedTextId");
-
-      if (commentWriteInput[index].value) {
-        div.innerText = commentWriteInput[index].value;
-        div.classList.add("feedTextContent");
-      }
-
-      li.appendChild(commentUserId);
-      li.appendChild(div);
-
-      commentList[index].appendChild(li);
-
-      commentWriteInput[index].value = "";
-
-      commentWriteButton[index].classList.remove("comment-btn");
-      commentWriteButton[index].disabled = true;
+      writeComment(index);
     }
   });
 });
 
-//게시버튼 누른 후 게시버튼 다시 비활성화
-Array.from(commentWriteButton).forEach((value, index) => {
+//게시버튼 누르면 댓글달기
+Array.from(commentWriteInput).forEach((value, index) => {
   commentWriteButton[index].addEventListener("click", () => {
-    const gesi = commentWriteInput[index].value;
-
-    if (gesi) {
-      commentWriteButton[index].classList.remove("comment-btn");
-      commentWriteButton[index].disabled = true;
-    }
+    writeComment(index);
   });
 });
 
-//내가짠 json댓글로직
-// let comment_list;
-
-// fetch("./data/commentsData.json", { method: "GET" })
-//   .then((res) => res.json())
-//   .then((data) => {
-//     comment_list = data;
-
-//     comment_list.forEach((comment, index) => {
-//       const liElement = document.createElement("li");
-//       const userName = document.createElement("div");
-//       const userComment = document.createElement("div");
-
-//       const comments = document.getElementsByClassName("commentList")[index];
-
-//       userName.innerText = comment.userName;
-//       userName.className = "feedTextId";
-//       userComment.innerText = comment.content;
-//       userComment.className = "feedTextContent";
-//       liElement.className = "comment";
-
-//       liElement.append(userName, userComment);
-//       comments.append(liElement);
-//     });
-//   });
-
-//하은멘토님 로직
-//const commentList = document.getElementsByClassName("commentList");
-
+//mokdata 받아와서 fetch로 연결 후 forEach로 매 피드에 댓글작성
 fetch("./data/commentsData.json")
   .then((res) => res.json())
   .then((data) => {
@@ -97,38 +65,14 @@ fetch("./data/commentsData.json")
         let userComment = document.createElement("div");
 
         userName.innerText = comment.userName;
-        userName.className = "feedTextId";
         userComment.innerText = comment.content;
+        userName.className = "feedTextId";
         userComment.className = "feedTextContent";
         liElement.className = "comment";
+
         liElement.append(userName, userComment);
 
         commentList[i].append(liElement);
       }
     });
   });
-
-//게시버튼 누르면 인풋에 입력된 값대로 댓글달기
-Array.from(commentWriteInput).forEach((value, index) => {
-  commentWriteButton[index].addEventListener("click", () => {
-    const li = document.createElement("li");
-    const commentUserId = document.createElement("div");
-    const div = document.createElement("div");
-
-    li.classList.add("comment");
-    commentUserId.innerText = "kim_code";
-    commentUserId.classList.add("feedTextId");
-
-    if (commentWriteInput[index].value) {
-      div.innerText = commentWriteInput[index].value;
-      div.classList.add("feedTextContent");
-    }
-
-    li.appendChild(commentUserId);
-    li.appendChild(div);
-
-    commentList[index].appendChild(li);
-
-    commentWriteInput[index].value = "";
-  });
-});
