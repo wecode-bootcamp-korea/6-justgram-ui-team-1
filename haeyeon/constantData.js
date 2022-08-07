bdd46716d222878dd5628228eb2abd45111b8a66
연쁜 <yeon@yeonppeun-ui-MacBookPro.local>
2022-08-07 15:50:10 +0900

    ADD:MISSION4 진행 중

---
+++ haeyeon/constantData.js
@@ -0,0 +1,25 @@
+const COMMENT_LIST = [
+  {
+    id: 1,
+    userName: "wecode",
+    content: "Welcome to world best coding bootcamp!",
+    isLiked: true,
+  },
+
+  {
+    id: 2,
+    userName: "code Kim",
+    content: "Hi there",
+    isLiked: false,
+  },
+
+  {
+    id: 3,
+    userName: "jayPark",
+    content: "Hey.",
+    isLiked: false,
+  },
+
+];
+
+export default COMMENT_LIST;
--- haeyeon/js/main.js
+++ haeyeon/js/main.js
@@ -1,6 +1,55 @@
+const commentWrite = document.getElementsByClassName("comment-write-box")[0];
+const commentPost = document.getElementsByClassName("comment-post-button")[0];
 
-const cmdBox = document.getElementById("cmd-box");
-const cmdUpload = document.getElementById("comment-upload");
+const commentList = document.getElementsByClassName("feed-comment-list")[0];
+
+
+commentPost.addEventListener("click", () => {
+  const content = commentWrite.value;
+
+
+  //console.log(content);
+
+  // 자식요소 만들고
+  const commentDiv = document.createElement("div");
+  const nameSpan = document.createElement("span");
+  nameSpan.className = "comment-name";
+  nameSpan.textContent = "onhaeye";
+
+
+  //댓글 내용 붙이고
+  commentDiv.append(nameSpan, content);
+  commentList.appendChild(commentDiv);
+
+  commentWrite.value = null;
+});
+
+
+
+commentWrite.addEventListener("keyup", function (e) {
+  if (e.key === 'Enter') {
+    const content = commentWrite.value;
+    //console.log(content);
+
+    // 자식요소 만들고
+    const commentDiv = document.createElement("div");
+    const nameSpan = document.createElement("span");
+    nameSpan.className = "comment-name";
+    nameSpan.textContent = "onhaeye";
+
+
+    //댓글 내용 붙이고
+    commentDiv.append(nameSpan, content);
+    commentList.appendChild(commentDiv);
+
+    commentWrite.value = null;
+  }
+});
+
+
+
+// const cmdBox = document.getElementById("cmd-box");
+// const cmdUpload = document.getElementById("comment-upload");
 
 
 // // function onInput(event) {
@@ -15,28 +64,28 @@ const cmdUpload = document.getElementById("comment-upload");
 
 
 
-function writeComment() {
+// function writeComment() {
 
-  const divtag = document.createElement("div");
-  divtag.textContent = "i_hate_cucumber"
-  document.querySelector(".up-comment1").append(divtag);
+//   const divtag = document.createElement("div");
+//   divtag.textContent = "i_hate_cucumber"
+//   document.querySelector(".up-comments1").append(divtag);
 
-  const divtag1 = document.createElement("div");
-  divtag1.textContent = cmdBox.value;
-  document.querySelector(".up-comment2").append(divtag1);
+//   const divtag1 = document.createElement("div");
+//   divtag1.textContent = cmdBox.value;
+//   document.querySelector(".up-commens2").append(divtag1);
 
 
-};
+// };
 
 // 엔터키 눌렀을 때 댓글 추가
-cmdBox.addEventListener('keyup', function (e) {
-  if (e.key === 'Enter') {
-    writeComment();
-    cmdBox.value = null;
-  }
-});
+// cmdBox.addEventListener('keyup', function (e) {
+//   if (e.key === 'Enter') {
+//     writeComment();
+//     cmdBox.value = null;
+//   }
+// });
 
-// 게시버튼눌렀을때댓글추가
-cmdUpload.addEventListener('Click', function () {
-  writeComment();
-});
+// // 게시버튼눌렀을때댓글추가
+// cmdUpload.addEventListener('Click', function () {
+//   writeComment();
+// });
