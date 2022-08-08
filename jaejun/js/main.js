cc08151bb27e7cef005015ccb9cc870d036b9ac3
전하은 <hani@jeonha-eun-ui-MacBookPro.local>
2022-08-08 10:37:28 +0900

    add : MISSION 5

--- jaejun/js/main.js
+++ jaejun/js/main.js
@@ -1,31 +1,58 @@
-const commentWriteInput = document.getElementsByClassName("comment-write-input");
-const commentWriteButton = document.getElementsByClassName("comment-write-button");
+const commentWriteInput = document.getElementsByClassName(
+  "comment-write-input"
+);
+const commentWriteButton = document.getElementsByClassName(
+  "comment-write-button"
+);
 const commentList = document.getElementsByClassName("feed-comment-list");
 
-console.log(Array.from(commentWriteButton));
-Array.from(commentWriteButton).forEach((button, index) => {
+fetch("./data/comments.json")
+  .then((res) => res.json())
+  .then((data) => {
+    //데이터를 받아서 배열로 다시 할당하기
+    let commentArray = data.comments;
+    //배열에 담긴 댓글을 하나하나 반복문 돌기
+    commentArray.forEach((comment) => {
+      //먼저 commentList 반복문 돌면서 피드 하나하나에 댓글 하나씩 넣어주기
+      for (let i = 0; i < commentArray.length; i++) {
+        //댓글을 넣을 div
+        const commentElem = document.createElement("div");
+        //댓글 닉네임을 넣을 span
+        const nameSpan = document.createElement("span");
+        nameSpan.className = "comment-name";
+        nameSpan.textContent = comment.userName;
+        //댓글 내용을 넣어주기
+        const content = comment.content;
+        commentElem.append(nameSpan, content);
+        //피드 하나하나에 댓글 넣기
+        commentList[i].append(commentElem);
+      }
+    });
+  });
 
+// console.log(Array.from(commentWriteButton));
+Array.from(commentWriteButton).forEach((button, index) => {
   button.addEventListener("click", () => {
     const content = commentWriteInput[index].value;
-    
-    const childElem = document.createElement('div');
-    
-    const nameSpan = document.createElement('span');
+
+    const childElem = document.createElement("div");
+
+    const nameSpan = document.createElement("span");
     nameSpan.className = "comment-name";
-    nameSpan.textContent = '재준';
-  
+    nameSpan.textContent = "재준";
+
     childElem.append(nameSpan, content);
-  
+
     commentList[index].appendChild(childElem);
   });
 });
 
 // commentWriteButton.addEventListener("click", () => {
 //   const content = commentWriteInput.value;
-  
+
 //   // 자식요소를 만들고
 //   const childElem = document.createElement('div');
-  
+
 //   const nameSpan = document.createElement('span');
 //   nameSpan.className = "comment-name";
 //   nameSpan.textContent = '재준';
